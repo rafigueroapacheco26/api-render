6c75fdd2cece58415103994c7cff3d7c72df488c
Rodrigo <rafiguerapacheco26@gmail.com>
2026-06-17 12:14:48 +0200

    añadir nueva ruta y cors

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from "express";
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
 app.use(express.json())
 
 const PORT = process.env.PORT || 3000;
@@ -8,6 +10,14 @@ const PORT = process.env.PORT || 3000;
 app.get("/", (req, res) => {
   res.json({ mensaje: "Bienvenidos a nuestra API" });
 });
+
+app.get("/api", (req, res) => {
+ res.json({
+   nombre: "Rodrigo",
+   email: "rafigueroapacheco26@gmailcom",
+ });
+});
+
 
 app.listen(PORT, () => {
   console.log(`Servidor iniciado en puerto http://localhost:${PORT}`);
